62a4752c952b013ec77dc631667505165131a17a
Arthur Vasek <vasekas@gmail.com>
2014-03-08 09:12:05 -0800

    Update index.php
    change pathways

--- InventoryApp/index.php
+++ InventoryApp/index.php
@@ -1,19 +1,27 @@
-<?php 
+<?php
+
 session_start();
 
-if (isset($_SESSION['username']) && $_SESSION['permissions']==0) {
-	$_SESSION['logged_in_inventory_app_cs419'] = 1;
-	header('Location: http://web.engr.oregonstate.edu/~ashmorel/419/InventoryApp/admin/admin.php');
-} else if (isset($_SESSION['username']) && $_SESSION['permissions']==1) {
-	$_SESSION['logged_in_inventory_app_cs419'] = 1;
-	header('Location: http://web.engr.oregonstate.edu/~ashmorel/419/InventoryApp/user/user.php');
-} else {
-	$_SESSION['logged_in_inventory_app_cs419'] = 0;
-	unset($_SESSION['username']);
-	unset($_SESSION['password']);
-	unset($_SESSION['create_username']);
-	unset($_SESSION['create_password']);
-	unset($_SESSION['password_verify']);
-	header('Location: sign_in.php');
+$flag=false;
+
+if (isset($_SESSION["permissions"])) {
+    if (isset($_SESSION['username']) && $_SESSION['permissions'] == 0) {
+        $_SESSION['logged_in_inventory_app_cs419'] = 1;
+        header('Location: admin/admin.php');
+        $flag=true;
+    } else if (isset($_SESSION['username']) && $_SESSION['permissions'] == 1) {
+        $_SESSION['logged_in_inventory_app_cs419'] = 1;
+        header('Location: user/user.php');
+        $flag=true;
+    }
+} 
+if(!$flag){
+    $_SESSION['logged_in_inventory_app_cs419'] = 0;
+    unset($_SESSION['username']);
+    unset($_SESSION['password']);
+    unset($_SESSION['create_username']);
+    unset($_SESSION['create_password']);
+    unset($_SESSION['password_verify']);
+    header('Location: ./signin/sign_in.php');
 }
 ?>
